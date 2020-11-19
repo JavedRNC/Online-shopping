0551bfb5da58ff05d8094f3ef66a45dbec03a8c3
Javed <javedansarirnc@gmail.com>
2020-11-19 10:35:00 +0530

    Loaded static resources

--- online-shopping/src/main/java/com/mjansari/onlineshopping/PageController.java
+++ online-shopping/src/main/java/com/mjansari/onlineshopping/PageController.java
@@ -1,6 +1,7 @@
 package com.mjansari.onlineshopping;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -12,6 +13,14 @@ public class PageController {
 	{
 		ModelAndView mv = new ModelAndView("page");
 		mv.addObject("greeting","Welcome");
+		return mv;
+	}
+	
+	@RequestMapping("test/{greeting}")
+	public ModelAndView test(@PathVariable(value = "greeting")String greeting)
+	{
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", greeting);
 		return mv;
 	}
 }
